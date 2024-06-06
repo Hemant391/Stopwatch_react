d5a4e3ea94a1749a17cc656d3221ab916e408985
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-06-06 18:14:52 +0530

    main code

---
+++ stopwatch/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+
+function App() {
+  const [running,setRunning]=useState(false)
+  const [time,setTime]=useState(0);
+  
+useEffect(()=>{
+  let interval;
+  if(running){
+    interval=setInterval(()=>setTime(time+1),10)
+  }
+  return ()=>clearInterval(interval)
+},[running,time])
+
+const minutes = Math.floor((time % 360000) / 6000);
+
+const seconds = Math.floor((time % 6000) / 100);
+  const handlerun=()=>{
+    setRunning(!running)
+
+}
+
+  const resettime=()=>{
+    setTime(0)
+  }
+  return (
+    <div className="App">
+     <h1>Stopwatch</h1>
+     <p><span>Time: </span><span>{minutes.toString().padStart(1,"0")}:{seconds.toString().padStart(2,'0')}</span></p>
+     <button type="button" onClick={handlerun}>{running?'Stop':'Start'}</button>
+     <button onClick={resettime}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
